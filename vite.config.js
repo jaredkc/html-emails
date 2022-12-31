521e00d5edbd9fffef2e4bdf370fc9ca7c592d89
jaredkc <jared@clivecoffee.com>
2022-12-31 16:03:44 -0700

    build all HTML files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { resolve } from 'path';
+import path from 'path';
+import glob from 'glob';
+
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+  root: path.join(__dirname, 'src'),
+  build: {
+    outDir: path.join(__dirname, 'dist'),
+    rollupOptions: {
+      input: glob.sync(path.resolve(__dirname, 'src', '*.html')),
+    },
+  },
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, 'partials'),
+      partialDirectory: resolve(__dirname, 'src/partials'),
     }),
   ],
 };
